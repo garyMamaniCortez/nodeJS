852e120dcaafbb31db71da760fd02557739aff9c
garyMamaniCortez <mamanijhonny2210@gmail.com>
2021-07-19 11:22:26 -0400

    diference between ES5 and ES6

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
-function saludar(nombre){
-    console.log("Hola " + nombre);
+function sayHi(name){
+    console.log("Hi " + name);
 }
-saludar('Gary');
+sayHi('Gary');
+
+// with ES5
+var names = "Jhonny Gary";
+console.log(names);
+names = "jhonny";
+// you can change it
+// with ES6
+const name1= "Gary";
+console.log(name1);
+//name1 = "Jhonny"; //you can't do this because const is only for read
+let years = 19;
+console.log(years);
+years = 20;
+// let is like var
